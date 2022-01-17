925c56c0739b676eb4b629fb4d3ccb920e93e07f
Sabrina-Ibrahim <Sabrina.ibrahim24@me.com>
2022-01-17 17:08:30 -0600

    Update styles and make sure project is working properly for searches.

--- src/components/MusicTable/MusicTable.jsx
+++ src/components/MusicTable/MusicTable.jsx
@@ -4,13 +4,13 @@ import './MusicTable.css';
 const MusicTable = (props) => {
   return (
     <div className="MusicTable">
-      <div className="container table">
+      <div className="container table music">
         {/* song.title=Drive My Car+>drive my car includes(props.search=dRiVe my car>drive my car) */}
         {props.music.filter((song) => (song.title.toLowerCase().includes(props.search.toLowerCase()) || song.artist.includes(props.search) || song.album.includes(props.search) || song.genre.includes(props.search) || song.releaseDate.includes(props.search))).map((song) => {
           return (
-            <div className="row row-cols-4" key={song.title}>
-              {/* add artist */}
+            <div className="row row-cols-5" key={song.title}>
               <div className="col">{song.title}</div>
+              <div className="col">{song.artist}</div>
               <div className="col">{song.album}</div>
               <div className="col">{song.genre}</div>
               <div className="col">{song.releaseDate}</div>
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -4,12 +4,24 @@ import "./NavigationBar.css"
 function NavigationBar() {
     return (
         <div className="card">
-            <div className="card-body navbar">
-                The Song Name
+            <div className="name">
+                <p><strong>This Song Name</strong></p>
+                <div className="artist">
+                    <p>The Author - Album</p></div></div>
+
+            <div className="container">
+                <div className="row row-cols-5">
+                    <div className="col"><strong>Title</strong></div>
+                    <div className="col"><strong>Artist</strong></div>
+                    <div className="col"><strong>Album</strong></div>
+                    <div className="col"><strong>Genre</strong></div>
+                    <div className="col"><strong>Release Date</strong></div>
+                </div>
+
             </div>
-            <p>The Author - Album</p>
+
         </div>
     );
 }
 
-export default NavigationBar;
+export default NavigationBar; 
